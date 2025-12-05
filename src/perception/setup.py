4751df6670effd2fe664ffad647dd4b76a856336
min_5214 <min_5214@snu.ac.kr>
2025-12-05 14:36:35 +0900

    perception module setup

--- src/perception/setup.py
+++ src/perception/setup.py
@@ -9,15 +9,14 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         # 1. Install Launch Files
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
         # 2. Install Config Files
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         # 3. Install Model Files
-        (os.path.join('share', package_name, 'models'), glob('models/*.pt')),
+        (os.path.join('share', package_name, 'model'), glob('model/*.pt')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
